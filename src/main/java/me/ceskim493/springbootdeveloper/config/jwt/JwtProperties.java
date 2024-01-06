93ea7e47a149fa7d0c3c79735501568ada42456c
Hwanpago0107 <ceskim493@naver.com>
2024-01-07 00:37:29 +0900

    delete secret info

--- src/main/java/me/ceskim493/springbootdeveloper/config/jwt/JwtProperties.java
+++ src/main/java/me/ceskim493/springbootdeveloper/config/jwt/JwtProperties.java
@@ -2,14 +2,17 @@ package me.ceskim493.springbootdeveloper.config.jwt;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Setter
 @Getter
 @Component
-@ConfigurationProperties("jwt")
+@PropertySource("classpath:/secure.properties")
 public class JwtProperties {
+    @Value("${jwt.issuer}")
     private String issuer;
+    @Value("${jwt.secret_key}")
     private String secretKey;
 }
