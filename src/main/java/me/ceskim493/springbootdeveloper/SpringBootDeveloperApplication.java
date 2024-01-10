2c86a676ac898b742e30aa1084ecb08b71944743
Hwanpago0107 <ceskim493@naver.com>
2024-01-10 21:46:17 +0900

    divide config

--- src/main/java/me/ceskim493/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/ceskim493/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -2,12 +2,10 @@ package me.ceskim493.springbootdeveloper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@PropertySource("classpath:/application-secret.properties")
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
