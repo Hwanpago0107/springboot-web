c2518fa3a2e5071e0c93a756a6962f2262a6c81c
Hwanpago0107 <ceskim493@naver.com>
2024-01-10 18:13:43 +0900

    test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO article (title, content, author, created_at, updated_at) values ('제목 1', '내용 1', '사용자 1', NOW(), NOW());
-INSERT INTO article (title, content, author, created_at, updated_at) values ('제목 2', '내용 2', '사용자 2',  NOW(), NOW());
-INSERT INTO article (title, content, author, created_at, updated_at) values ('제목 3', '내용 3', '사용자 3', NOW(), NOW());
+INSERT INTO article (title, content, author, created_at, updated_at) values ('제목 111', '내용 1', '사용자 1', NOW(), NOW());
+INSERT INTO article (title, content, author, created_at, updated_at) values ('제목 222', '내용 2', '사용자 2',  NOW(), NOW());
+INSERT INTO article (title, content, author, created_at, updated_at) values ('제목 333', '내용 3', '사용자 3', NOW(), NOW());
